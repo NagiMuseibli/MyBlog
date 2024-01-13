61a723df05baae1b8c3567ba28fb1b6605427303
NagiMuseibli <nagimuseibli02@gmail.com>
2024-01-13 23:04:13 +0400

    Showing datas in blog.index view

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -26,86 +26,26 @@
             <div class="col-md-8 blog-posts">
                 <div class="row">
                     <div class="col-md-12">
-                        <div class="post-blog">
-                            <div class="blog-image">
-                                <a href="blog-single.html">
-                                    <img src="{{ 'assets/images/includes/blog1.jpg' }}" alt="">
-                                </a>
-                            </div> <!-- /.blog-image -->
-                            <div class="blog-content">
-                                <span class="meta-date"><a href="#">28 February 2084</a></span>
-                                <span class="meta-comments"><a href="#">14 Comments</a></span>
-                                <span class="meta-author"><a href="#">Candy Sharp</a></span>
-                                <h3><a href="blog-single.html">Getting Creative With the Google Maps API</a></h3>
-                                <p class="light-text">Sed consequat lobortis risus, in rutrum arcu tristique eget. Etiam
-                                    accumsan lectus quis cursus porta. Etiam a turpis sed sapien malesuada pellentesque quis
-                                    id tortor. Phasellus mattis quam enim, non accumsan nibh tincidunt sed.</p>
-                                <p>Nunc ullamcorper nisi sit amet eros dictum, eget vulputate quam rhoncus. Nulla diam eros,
-                                    ultrices id lacinia ut, aliquet sit amet erat. Duis ut nulla molestie, ullamcorper
-                                    ligula eu, ultricies tortor. Suspendisse quis consequat a turpis sed sapien quis id
-                                    tortor <a href="blog-single.html">Continue Reading...</a></p>
-                            </div> <!-- /.blog-content -->
-                        </div> <!-- /.post-blog -->
-                        <div class="post-blog">
-                            <div class="blog-image">
-                                <a href="blog-single.html">
-                                    <img src="{{ 'assets/images/includes/blog2.jpg' }}" alt="">
-                                </a>
-                            </div> <!-- /.blog-image -->
-                            <div class="blog-content">
-                                <span class="meta-date"><a href="#">24 February 2084</a></span>
-                                <span class="meta-comments"><a href="#">32 Comments</a></span>
-                                <span class="meta-author"><a href="#">Candy Sharp</a></span>
-                                <h3><a href="blog-single.html">Bird Profile Wellington New Zealand</a></h3>
-                                <p class="light-text">Sed consequat lobortis risus, in rutrum arcu tristique eget. Etiam
-                                    accumsan lectus quis cursus porta. Etiam a turpis sed sapien malesuada pellentesque quis
-                                    id tortor. Phasellus mattis quam enim, non accumsan nibh tincidunt sed.</p>
-                                <p>Nunc ullamcorper nisi sit amet eros dictum, eget vulputate quam rhoncus. Nulla diam eros,
-                                    ultrices id lacinia ut, aliquet sit amet erat. Duis ut nulla molestie, ullamcorper
-                                    ligula eu, ultricies tortor. Suspendisse quis consequat a turpis sed sapien quis id
-                                    tortor <a href="blog-single.html">Continue Reading...</a></p>
-                            </div> <!-- /.blog-content -->
-                        </div> <!-- /.post-blog -->
-                        <div class="post-blog">
-                            <div class="blog-image">
-                                <a href="blog-single.html">
-                                    <img src="{{ 'assets/images/includes/blog3.jpg' }}" alt="">
-                                </a>
-                            </div> <!-- /.blog-image -->
-                            <div class="blog-content">
-                                <span class="meta-date"><a href="#">20 February 2084</a></span>
-                                <span class="meta-comments"><a href="#">64 Comments</a></span>
-                                <span class="meta-author"><a href="#">Candy Sharp</a></span>
-                                <h3><a href="blog-single.html">Standard Blog Post Formating Medigo</a></h3>
-                                <p class="light-text">Sed consequat lobortis risus, in rutrum arcu tristique eget. Etiam
-                                    accumsan lectus quis cursus porta. Etiam a turpis sed sapien malesuada pellentesque quis
-                                    id tortor. Phasellus mattis quam enim, non accumsan nibh tincidunt sed.</p>
-                                <p>Nunc ullamcorper nisi sit amet eros dictum, eget vulputate quam rhoncus. Nulla diam eros,
-                                    ultrices id lacinia ut, aliquet sit amet erat. Duis ut nulla molestie, ullamcorper
-                                    ligula eu, ultricies tortor. Suspendisse quis consequat a turpis sed sapien quis id
-                                    tortor <a href="blog-single.html">Continue Reading...</a></p>
-                            </div> <!-- /.blog-content -->
-                        </div> <!-- /.post-blog -->
-                        <div class="post-blog">
-                            <div class="blog-image">
-                                <a href="blog-single.html">
-                                    <img src="{{ 'assets/images/includes/blog4.jpg' }}" alt="">
-                                </a>
-                            </div> <!-- /.blog-image -->
-                            <div class="blog-content">
-                                <span class="meta-date"><a href="#">24 February 2084</a></span>
-                                <span class="meta-comments"><a href="#">128 Comments</a></span>
-                                <span class="meta-author"><a href="#">Candy Sharp</a></span>
-                                <h3><a href="blog-single.html">Hochbunker High Definition Photography</a></h3>
-                                <p class="light-text">Sed consequat lobortis risus, in rutrum arcu tristique eget. Etiam
-                                    accumsan lectus quis cursus porta. Etiam a turpis sed sapien malesuada pellentesque quis
-                                    id tortor. Phasellus mattis quam enim, non accumsan nibh tincidunt sed.</p>
-                                <p>Nunc ullamcorper nisi sit amet eros dictum, eget vulputate quam rhoncus. Nulla diam eros,
-                                    ultrices id lacinia ut, aliquet sit amet erat. Duis ut nulla molestie, ullamcorper
-                                    ligula eu, ultricies tortor. Suspendisse quis consequat a turpis sed sapien quis id
-                                    tortor <a href="blog-single.html">Continue Reading...</a></p>
-                            </div> <!-- /.blog-content -->
-                        </div> <!-- /.post-blog -->
+                        @foreach ($posts as $post)
+                            <div class="post-blog">
+                                <div class="blog-image">
+                                    <a href="blog-single.html">
+                                        <img src="{{ 'assets/images/includes/blog1.jpg' }}" alt="">
+                                    </a>
+                                </div> <!-- /.blog-image -->
+                                <div class="blog-content">
+                                    <span class="meta-date"><a href="#">28 February 2084</a></span>
+                                    <span class="meta-comments"><a href="#">14 Comments</a></span>
+                                    <span class="meta-author"><a href="#">Candy Sharp</a></span>
+                                    <h3><a href="blog-single.html">{{ $post->title }}</a></h3>
+                                    <p class="light-text">{{ $post->excerpt }}</p>
+                                    <p>{{ $post->body }}<a href="{{ route('blog.show', $post->id) }}">Continue
+                                            Reading...</a></p>
+                                </div> <!-- /.blog-content -->
+                            </div> <!-- /.post-blog -->
+                        @endforeach
+
+
                     </div> <!-- /.col-md-12 -->
                     <div class="col-md-12">
                         <ul class="pages">
