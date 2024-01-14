4e67860fe03594cf4113b1043c50b64653941b56
NagiMuseibli <nagimuseibli02@gmail.com>
2024-01-15 01:32:59 +0400

    Latest posts adding to blog.index blade

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -64,69 +64,22 @@
                 <div class="sidebar">
                     <div class="sidebar-widget">
                         <h5 class="widget-title">Recent Posts</h5>
-                        <div class="last-post clearfix">
-                            <div class="thumb pull-left">
-                                <a href="#"><img src="{{ 'assets/images/includes/blogthumb1.jpg' }}"
-                                        alt=""></a>
-                            </div>
-                            <div class="content">
-                                <span>24 February 2084</span>
-                                <h4><a href="#">Standard Post Formating Medigo</a></h4>
-                            </div>
-                        </div> <!-- /.last-post -->
-                        <div class="last-post clearfix">
-                            <div class="thumb pull-left">
-                                <a href="#"><img src="{{ 'assets/images/includes/blogthumb2.jpg' }}"
-                                        alt=""></a>
-                            </div>
-                            <div class="content">
-                                <span>24 February 2084</span>
-                                <h4><a href="#">Standard Post Formating Medigo</a></h4>
-                            </div>
-                        </div> <!-- /.last-post -->
-                        <div class="last-post clearfix">
-                            <div class="thumb pull-left">
-                                <a href="#"><img src="{{ 'assets/images/includes/blogthumb3.jpg' }}"
-                                        alt=""></a>
-                            </div>
-                            <div class="content">
-                                <span>24 February 2084</span>
-                                <h4><a href="#">Standard Post Formating Medigo</a></h4>
-                            </div>
-                        </div> <!-- /.last-post -->
-                    </div> <!-- /.sidebar-widget -->
-                    <div class="sidebar-widget">
-                        <h5 class="widget-title">Categories</h5>
-                        <div class="row categories">
-                            <div class="col-md-6">
-                                <ul>
-                                    <li><a href="#">Standard</a></li>
-                                    <li><a href="#">Audio</a></li>
-                                    <li><a href="#">Video</a></li>
-                                    <li><a href="#">Branding</a></li>
-                                </ul>
-                            </div>
-                            <div class="col-md-6">
-                                <ul>
-                                    <li><a href="#">iOS Design</a></li>
-                                    <li><a href="#">Business</a></li>
-                                </ul>
-                            </div>
-                        </div> <!-- /.row -->
-                    </div> <!-- /.sidebar-widget -->
-                    <div class="sidebar-widget">
-                        <h5 class="widget-title">Flickr Feed</h5>
-                        <ul id="flickr-feed" class="thumbs"></ul>
-                    </div> <!-- /.sidebar-widget -->
-                    <div class="sidebar-widget">
-                        <h5 class="widget-title">Text Widget</h5>
-                        <p class="light-text">Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut
-                            fermentum massa justo sit amet risus. Cum sociis natoque penatibus et magnis dis parturient…
-                        </p>
-                    </div> <!-- /.sidebar-widget -->
-                </div> <!-- /.sidebar -->
-            </div> <!-- /.col-md-4 -->
+                        @foreach ($latestPosts as $latestPost)
+                            <div class="last-post clearfix">
+                                <div class="thumb pull-left">
+                                    <a href="{{ route('blog.show', $latestPost->id) }}"><img
+                                            src="{{ 'assets/images/includes/blogthumb1.jpg' }}" alt=""></a>
+                                </div>
+                                <div class="content">
+                                    <span>{{ $latestPost->created_at->format('d/m/Y') }}</span>
+                                    <h4><a href="{{ route('blog.show', $latestPost->id) }}">{{ $latestPost->title }}</a>
+                                    </h4>
+                                </div>
+                            </div> <!-- /.last-post -->
+                        @endforeach
+                    </div> <!-- /.sidebar -->
+                </div> <!-- /.col-md-4 -->
 
-        </div> <!-- /.row -->
-    </div> <!-- /.container -->
-@endsection
+            </div> <!-- /.row -->
+        </div> <!-- /.container -->
+    @endsection
