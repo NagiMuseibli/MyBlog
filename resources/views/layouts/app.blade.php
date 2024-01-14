1e61f595b50727d5f7410a375e20a9f5ac6bf766
NagiMuseibli <nagimuseibli02@gmail.com>
2024-01-15 01:28:39 +0400

    Editing on Header  Front

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,20 +34,10 @@
     <div class="responsive_menu">
         <ul class="main_menu">
             <li><a href="{{ route('home') }}">Home</a></li>
-            <li><a href="#">Portfolio</a>
-                <ul>
-                    <li><a href="portfolio.html">Portfolio Grid</a></li>
-                    <li><a href="project-image.html">Project Image</a></li>
-                    <li><a href="project-slideshow.html">Project Slideshow</a></li>
-                </ul>
-            </li>
-            <li><a href="#">Blog</a>
-                <ul>
-                    <li><a href="blog.html">Blog Standard</a></li>
-                    <li><a href="blog-single.html">Blog Single</a></li>
-                    <li><a href="#">visit templatemo</a></li>
-                </ul>
-            </li>
+            <li><a href="#">Portfolio</a></li>
+
+            <li><a href="{{ route('blog.index') }}">Blog</a></li>
+
             <li><a href="archives.html">Archives</a></li>
             <li><a href="contact.html">Contact</a></li>
         </ul> <!-- /.main_menu -->
@@ -71,20 +61,8 @@
                         <nav class="main-nav visible-md visible-lg">
                             <ul class="sf-menu">
                                 <li class="active"><a href="{{ route('home') }}">Home</a></li>
-                                <li><a href="#">Portfolio</a>
-                                    <ul>
-                                        <li><a href="portfolio.html">Portfolio Grid</a></li>
-                                        <li><a href="project-image.html">Project Image</a></li>
-                                        <li><a href="project-slideshow.html">Project Slideshow</a></li>
-                                    </ul>
-                                </li>
-                                <li><a href="#">Blog</a>
-                                    <ul>
-                                        <li><a href="{{ route('blog.index') }}">Blog Standard</a></li>
-                                        <li><a href="blog-single.html">Blog Single</a></li>
-                                        <li><a href="#">visit templatemo</a></li>
-                                    </ul>
-                                </li>
+                                <li><a href="#">Portfolio</a> </li>
+                                <li><a href="{{ route('blog.index') }}">Blog</a> </li>
                                 <li><a href="archives.html">Archives</a></li>
                                 <li><a href="contact.html">Contact</a></li>
                             </ul> <!-- /.sf-menu -->
